2926666be0abfb9c6206f5c14d0669cb29b93a1d
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2024-04-05 21:07:47 +0100

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Adam Carter on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
